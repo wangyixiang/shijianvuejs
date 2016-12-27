e741acff3a36934ab547cb1866b5e7b0a41e232f
wangyixiang <wangyixiang.linux@qq.com>
2016-12-27 23:01:29 +0800

    adding github-commit practise.

---
+++ github-commit/src/index.js
@@ -0,0 +1,52 @@
+/**
+ * Created by nemo on 2016/12/25.
+ */
+import Vue from 'vue';
+import $ from 'jquery';
+
+const apiURL = 'https://api.github.com/repos/vuejs/vue/commits?per_page=3&sha=';
+
+new Vue({
+    el: '#app',
+    data: {
+        getDataMethods: {},
+        getDataMethod: null,
+        branches: ['master', 'dev'],
+        currentBranch: 'master',
+        commits: []
+    },
+    created: function () {
+        this.getDataMethods["fetchData"] = this.fetchData;
+        this.getDataMethods["fetchData$"] = this.fetchData$
+        this.getDataMethod = Object.keys(this.getDataMethods)[0]
+        this.getDataMethods[this.getDataMethod]()
+    },
+    watch: {
+        currentBranch: function () {
+            this.getDataMethods[this.getDataMethod]();
+        }
+    },
+    methods: {
+        fetchData: function () {
+            let xhr = new XMLHttpRequest();
+            let self = this
+            xhr.open('GET', apiURL + this.currentBranch);
+            xhr.onload = function () {
+                self.commits = JSON.parse(xhr.responseText);
+            };
+            xhr.send();
+            console.log("using fetchData.")
+        },
+        fetchData$: function () {
+            let self = this;
+            $.get(apiURL + this.currentBranch, function (data) {
+                self.commits.splice(data.length);
+                for (let i = 0; i < data.length; i++) {
+                    Vue.set(self.commits, i, data[i]);
+                }
+            }, 'json');
+            console.log("using fetchData$");
+        }
+    }
+});
+
