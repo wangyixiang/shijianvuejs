f6225498ec9a182422f424e420f17a971e6cf0ae
wangyixiang <wangyixiang.linux@qq.com>
2016-12-28 11:51:27 +0800

    manipulate the instance data member through standard way.

--- github-commit/src/index.js
+++ github-commit/src/index.js
@@ -18,7 +18,7 @@ new Vue({
     created: function () {
         this.getDataMethods["fetchData"] = this.fetchData;
         this.getDataMethods["fetchData$"] = this.fetchData$
-        this.getDataMethod = Object.keys(this.getDataMethods)[0]
+        this.getDataMethod = Object.keys(this.getDataMethods)[1]
         this.getDataMethods[this.getDataMethod]()
     },
     watch: {
@@ -33,6 +33,7 @@ new Vue({
             xhr.open('GET', apiURL + this.currentBranch);
             xhr.onload = function () {
                 self.commits = JSON.parse(xhr.responseText);
+                console.log(self.commits)
             };
             xhr.send();
             console.log("using fetchData.")
@@ -40,10 +41,15 @@ new Vue({
         fetchData$: function () {
             let self = this;
             $.get(apiURL + this.currentBranch, function (data) {
-                self.commits.splice(data.length);
-                for (let i = 0; i < data.length; i++) {
-                    Vue.set(self.commits, i, data[i]);
-                }
+                // self.commits.splice(data.length);
+                // for (let i = 0; i < data.length; i++) {
+                //     Vue.set(self.commits, i, data[i]);
+                // }
+                // 在Vue的数组操作中有两个限制, 一个是对array的index操作, 还有一个就是为数组设置新长度, Observer观察不到
+                // 所以上面测试一下.
+                console.log(data);
+                self.commits = data;
+                console.log(self.commits);
             }, 'json');
             console.log("using fetchData$");
         }
