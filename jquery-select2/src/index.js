b13984ad4026eefda0469e3507bb033d3b1a1911
wangyixiang <wangyixiang.linux@qq.com>
2016-12-31 12:35:28 +0800

    利用jquery, 实现一个支持value的组件, 从而使他还支持v-model, v-model只是语法糖哦.

--- jquery-select2/src/index.js
+++ jquery-select2/src/index.js
@@ -3,23 +3,28 @@
  */
 import Vue from 'vue';
 Vue.config.devtools = true;
+import $ from 'jquery';
 
 
 Vue.component('VueSelect2', {
     template: '#vue-select2',
     props: [
         'options',
+        'value'
     ],
-    methods:{
-        // 在Dom上的原生事件是会以一个事件对象传给callback的, 这就是为什么这个input会有一个Event对象.
-        log: function (event) {
-            // 在Vue中父组件通过设置prop来来把其状态设置给子组件, 反之不推荐的, 也就是说子组件是不可以设置prop来把
-            // 状态回置到父组件的. 总结叫做, prop down, event up.
-            console.log(event);
-            console.log(arguments);
-            // 而自定义的事件,只会把extra data闯过来, 而这个自定义的input事件$emit后, 对应的handler只有extra data.
-            this.$emit(event.type, event.target.value,'wangyixiang');
+    mounted: function () {
+        let self = this;
+        // 在VNode挂载到了Dom tree中后, jquery就可以登场了
+        for (let option of this.options) {
+            console.log(option);
+            $(this.$el).append(`<option value=${option.value}>${option.name}</option>`);
         }
+        $(this.$el).on('change', function () {
+            // 这里的this真得是容易和外面的this相混淆,这个this不是
+            // Vue的instance, 也不是别的函数的上下文, 而是jquery里面的
+            // 与函数绑定的Dom对象, 而不是jquery对象哦.
+            self.$emit('input', this.value);
+        });
     }
 });
 
@@ -30,16 +35,9 @@ new Vue({
         optiondata:[
             {name: 'name1', value: '1'},
             {name: 'name2', value: '2'},
-            {name: 'name3', value: '3'}
+            {name: 'name3', value: '3'},
         ],
         selected: 0
-    },
-    methods: {
-        // 这个就是对应的自定义的input时间的处理
-        valuechanged: function () {
-            console.log(arguments);
-            this.selected = arguments[0];
-        }
     }
 });
 
